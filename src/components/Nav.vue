ec1f97382da1a2de354d7478a0a01f8fe4ef2d82
Sid Pagariya <sidp@umich.edu>
2020-10-25 17:03:21 -0400

    Add Vuetify, basic App layout, and dark mode toggle

---
+++ src/components/Nav.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar elevate-on-scroll app>
+    <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon @click="toggleDark">
+      <v-icon>mdi-theme-light-dark</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppNav',
+  data() {
+    return {
+      appTitle: "Sid's Quick Links",
+    }
+  },
+  methods: {
+    toggleDark: function() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      localStorage.setItem('dark_theme', this.$vuetify.theme.dark.toString())
+    },
+  },
+  mounted() {
+    const theme = localStorage.getItem('dark_theme')
+    if (theme) {
+      if (theme == 'true') {
+        this.$vuetify.theme.dark = true
+      } else {
+        this.$vuetify.theme.dark = false
+      }
+    }
+  },
+}
+</script>
+
+<style scoped></style>
