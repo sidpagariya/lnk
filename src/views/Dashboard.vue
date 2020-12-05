0490385731f533f3b3eb7924ef35c680b11561b1
Sid Pagariya <sidp@umich.edu>
2020-12-05 01:21:45 -0500

    Add direct link to shortlink as well as copy button to copy shortlink

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -23,11 +23,18 @@
           :search="search"
           no-data-text="No links found!"
         >
+          <template v-slot:[`item.shortlink`]="{ item }">
+            <router-link :to="item.shortlink">{{ item.shortlink }}</router-link>
+          </template>
+
           <template v-slot:[`item.actions`]="{ item }">
             <v-icon small class="mr-2" @click="editItem(item)">
               mdi-pencil
             </v-icon>
             <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+            <v-icon small class="ml-2" @click="copyToClip(item.shortlink)">
+              mdi-content-copy
+            </v-icon>
           </template>
         </v-data-table>
       </v-card>
@@ -125,7 +132,13 @@ export default {
     headers: [
       { text: 'Short Link', value: 'shortlink', align: 'end' },
       { text: 'Full URL', value: 'url' },
-      { text: 'Actions', value: 'actions', sortable: false },
+      {
+        text: 'Actions',
+        value: 'actions',
+        width: '96px',
+        align: 'center',
+        sortable: false,
+      },
     ],
     links: [],
     dialog: false,
@@ -199,6 +212,67 @@ export default {
       this.editedIndex = this.links.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialogDelete = true
+    },
+    copyToClip(text) {
+      console.log(`${window.location.origin}/${text}`)
+      var textArea = document.createElement('textarea')
+
+      //
+      // *** This styling is an extra step which is likely not required. ***
+      //
+      // Why is it here? To ensure:
+      // 1. the element is able to have focus and selection.
+      // 2. if the element was to flash render it has minimal visual impact.
+      // 3. less flakyness with selection and copying which **might** occur if
+      //    the textarea element is not visible.
+      //
+      // The likelihood is the element won't even render, not even a
+      // flash, so some of these are just precautions. However in
+      // Internet Explorer the element is visible whilst the popup
+      // box asking the user for permission for the web page to
+      // copy to the clipboard.
+      //
+
+      // Place in the top-left corner of screen regardless of scroll position.
+      textArea.style.position = 'fixed'
+      textArea.style.top = 0
+      textArea.style.left = 0
+
+      // Ensure it has a small width and height. Setting to 1px / 1em
+      // doesn't work as this gives a negative w/h on some browsers.
+      textArea.style.width = '2em'
+      textArea.style.height = '2em'
+
+      // We don't need padding, reducing the size if it does flash render.
+      textArea.style.padding = 0
+
+      // Clean up any borders.
+      textArea.style.border = 'none'
+      textArea.style.outline = 'none'
+      textArea.style.boxShadow = 'none'
+
+      // Avoid flash of the white box if rendered for any reason.
+      textArea.style.background = 'transparent'
+
+      textArea.value = `${window.location.origin}/${text}`
+
+      document.body.appendChild(textArea)
+      textArea.focus()
+      textArea.select()
+
+      try {
+        var successful = document.execCommand('copy')
+        if (!successful) throw 'unsuccessful copy'
+        this.snackbarText = `Successfully copied shortlink ${text}!`
+        this.snackbarType = 'success'
+        this.showSnackbar = true
+      } catch (err) {
+        this.snackbarText = `Error copying shortlink ${text}!`
+        this.snackbarType = 'error'
+        this.showSnackbar = true
+      }
+
+      document.body.removeChild(textArea)
     },
     deleteItemConfirm() {
       var batch = db.batch()
